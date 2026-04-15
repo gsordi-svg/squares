048cd993a841b752bb3777d5fb75a2af7b04f291
Gabriella Sordi <gabriellasordi@Gabriellas-MacBook-Air.local>
2026-04-15 10:40:10 -0400

    Made a function to add random amount of square on screen and give them a random color and position.

---
+++ squares.js
@@ -0,0 +1,37 @@
+let squareArea = document.querySelector("#squarearea");
+//let squareArea = document.getElementById("squarearea");
+let squareCount = parseInt(Math.random()*21)+30; //30-50
+
+for (let i =0;i<squareCount;i++){
+    //make a square
+    let square = document.createElement("div")
+    square.className = "square";
+    square.style.left = parseInt(Math.random()*650)+"px";
+    square.style.top = parseInt(Math.random()*250)+"px";
+    square.style.backgroundColor = getRandomColor();
+    //I can put a onclick and a function(this)
+    //put it on the screen
+    squareArea.append(square);
+}
+
+function getRandomColor(){
+    //Make a Color string?
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0;i <6;i++){
+        color+=letters.charAt(parseInt(Math.random()*letters.length));
+    }
+    //return that string
+    return color;
+}
+//Make a function to add 1 square
+//Make sure that i set that as the onclick for thebutton
+
+//Make a Function that changes all the colors of all the squares
+//Make sure the button calls it
+function changeColors(){
+    //Make an array
+    let allSquares = document.querySelectorAll(".square");
+    //For of loop or just do a regular for loop with index
+    //Change the color
+}
